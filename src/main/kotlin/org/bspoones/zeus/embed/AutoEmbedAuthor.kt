88535fa0fd91bebdbb82f73f71ca01651aaf8386
BSpoones <76079263+BSpoones@users.noreply.github.com>
2024-03-26 01:07:31 +0000

    add: Nullable field values

--- src/main/kotlin/org/bspoones/zeus/embed/AutoEmbedAuthor.kt
+++ src/main/kotlin/org/bspoones/zeus/embed/AutoEmbedAuthor.kt
@@ -11,7 +11,7 @@ package org.bspoones.zeus.embed
  */
 class AutoEmbedAuthor(
     val name: String,
-    val url: String,
-    val iconUrl: String
+    val url: String?,
+    val iconUrl: String?
 ) {
 }
