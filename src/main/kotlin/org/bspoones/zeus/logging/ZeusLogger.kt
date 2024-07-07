0f77db639468bb21590f7029e4326da34507f1b9
BSpoones <76079263+BSpoones@users.noreply.github.com>
2024-07-07 19:05:38 +0100

    feat: Logger

---
+++ src/main/kotlin/org/bspoones/zeus/logging/ZeusLogger.kt
@@ -0,0 +1,28 @@
+package org.bspoones.zeus.logging
+
+import org.slf4j.LoggerFactory
+
+var SHOULD_LOG: Boolean = true
+
+fun getZeusLogger(name: String) = ZeusLogger(name)
+
+class ZeusLogger(name: String) {
+    private val logger = LoggerFactory.getLogger(name)
+
+    fun debug(msg: String) {
+        if (SHOULD_LOG) logger.debug(msg)
+    }
+
+    fun info(msg: String) {
+        if (SHOULD_LOG) logger.info(msg)
+    }
+    fun warn(msg: String) {
+        if (SHOULD_LOG) logger.warn(msg)
+    }
+    fun error(msg: String) {
+        if (SHOULD_LOG) logger.error(msg)
+    }
+    fun trace(msg: String) {
+        if (SHOULD_LOG) logger.trace(msg)
+    }
+}
