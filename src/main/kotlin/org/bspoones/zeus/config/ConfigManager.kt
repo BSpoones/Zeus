a186e93b650af08364e73a8c839f2887045eef66
BSpoones <76079263+BSpoones@users.noreply.github.com>
2024-07-08 00:05:12 +0100

    feat: Extras implementation

--- src/main/kotlin/org/bspoones/zeus/config/ConfigManager.kt
+++ src/main/kotlin/org/bspoones/zeus/config/ConfigManager.kt
@@ -1,36 +1,31 @@
 package org.bspoones.zeus.config
 
-import org.slf4j.LoggerFactory
+import org.bspoones.zeus.config.annotations.ConfigDirectory
 import kotlin.reflect.KClass
-import org.bspoones.zeus.*
 import org.bspoones.zeus.logging.getZeusLogger
 
-private val logger = getZeusLogger("$NAME | Config")
-
-fun <T : Any> getConfig(clazz: KClass<T>): T = ConfigManager.getConfig(clazz.java)
-inline fun <reified T : Any> getConfig(): T = getConfig(T::class)
-
+inline fun <reified T : Any> getConfig(): T = ConfigManager.getConfig(T::class.java)
 fun <T : Any> initConfig(vararg clazzes: KClass<T>) = clazzes.forEach { ConfigManager.initConfig(it.java) }
-fun <T : Any> initConfig(clazz: KClass<T>, directory: String = "") = ConfigManager.initConfig(clazz.java, directory)
-fun <T : Any> initConfig(map: Map<String, List<KClass<T>>>) = map.entries.forEach { (directory, classes) ->
-    classes.forEach { clazz ->
-        ConfigManager.initConfig(clazz.java, directory)
-    }
-}
 
-
+// TODO -> JavaDoc
 object ConfigManager : AutoCloseable {
+    private val logger = getZeusLogger("Config")
     private val configMap: MutableMap<Class<*>, ConfigFile<*>> = mutableMapOf()
 
     override fun close() {
         configMap.values.forEach {
+            logger.debug("Closing ${it.clazz.simpleName}.json")
             it.close()
         }
     }
 
-    fun <T> initConfig(config: Class<T>, directory: String = "") {
+    fun <T> initConfig(config: Class<T>) {
+        logger.debug("Initialising ${config.simpleName}")
+        val directory = if (config.isAnnotationPresent(ConfigDirectory::class.java)) {
+            config.getAnnotation(ConfigDirectory::class.java).directory
+        } else ""
+
         try {
-            logger.debug("Initialising ${config.simpleName}")
             configMap[config] = ConfigFile(config, directory)
         } catch (t: Throwable) {
             t.printStackTrace()
@@ -39,8 +34,8 @@ object ConfigManager : AutoCloseable {
 
     @Suppress("UNCHECKED_CAST")
     fun <T> getConfig(config: Class<T>): T {
-        return configMap[config]!!.getConfig() as T
+        logger.debug("Fetching config file of ${config.simpleName}")
+        return configMap[config]?.getConfig() as T
+            ?: throw ClassNotFoundException("The class ${config.simpleName} has not been initialised!")
     }
-
-
 }
