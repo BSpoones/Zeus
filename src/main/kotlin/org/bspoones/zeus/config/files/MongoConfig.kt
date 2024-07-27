0afbe7840fb9d3d8876e4e6fd491ae977c081c2b
BSpoones <76079263+BSpoones@users.noreply.github.com>
2024-07-27 01:19:25 +0100

    misc fixes

--- src/main/kotlin/org/bspoones/zeus/config/files/MongoConfig.kt
+++ src/main/kotlin/org/bspoones/zeus/config/files/MongoConfig.kt
@@ -4,19 +4,23 @@ import com.mongodb.ConnectionString
 import com.mongodb.MongoClientSettings
 import org.bspoones.zeus.config.annotations.ConfigDirectory
 import org.bspoones.zeus.config.base.ActionableConfig
+import org.bspoones.zeus.extensions.URLEncode
 import org.bspoones.zeus.logging.getZeusLogger
 import org.spongepowered.configurate.objectmapping.ConfigSerializable
 import java.net.URLEncoder
 import java.nio.file.WatchEvent
 import org.bspoones.zeus.storage.MongoConnection
+import org.bspoones.zeus.util.scheduling.delayTask
+import java.time.Duration
 
-private fun String.URLEncode() = URLEncoder.encode(this, "UTF-8")
 
 @ConfigSerializable
 @ConfigDirectory("zeus")
-internal class MongoConfig: ActionableConfig() {
+internal class MongoConfig : ActionableConfig() {
 
     var mongoEnabled: Boolean = true
+
+    var ssl: Boolean = true
 
     var address: String = "localhost"
     var port: Int = 27017
@@ -25,7 +29,7 @@ internal class MongoConfig: ActionableConfig() {
     var password: String = ""
 
     private fun connectionString(): String {
-        val x =  "mongodb://" +
+        return "mongodb://" +
                 username.URLEncode() +
                 ":" +
                 password.URLEncode() +
@@ -35,13 +39,7 @@ internal class MongoConfig: ActionableConfig() {
                 port +
                 "/" +
                 databaseName.URLEncode() +
-                "?ssl=false"
-
-        println("ADDRESS IS $x")
-
-        // TODO -> SSL this lol
-
-        return x
+                "?ssl=${ssl.toString().lowercase()}"
     }
 
     fun settings(): MongoClientSettings = MongoClientSettings.builder()
@@ -52,7 +50,10 @@ internal class MongoConfig: ActionableConfig() {
         val logger = getZeusLogger("MongoConfig")
 
         logger.info("MongoConfig has changed, restarting Mongo Connection!")
-        MongoConnection.setup()
+        // Ensuring config is loaded properly
+        delayTask(Duration.ofSeconds(1)) {
+            MongoConnection.setup()
+        }
     }
 
 }
